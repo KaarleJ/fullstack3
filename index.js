6557a419b271b54dd04c5b925a017e516abe2622
KaarleJ <117437182+KaarleJ@users.noreply.github.com>
2022-12-24 12:00:44 +0200

    3.14
    Ohjelma päivitetty vastaamaan osaa 3.14

--- index.js
+++ index.js
@@ -1,111 +1,89 @@
+require('dotenv').config()
 const { request, response } = require('express')
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const Person = require('./models/person.js')
 app.use(express.json())
 app.use(express.static('build'))
 
 morgan.token('data', (req, res) => JSON.stringify(req.body) )
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
-let persons = [
-    {
-        id : 1,
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id : 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-    },
-    {
-        id : 3,
-        name: "Dan Abramov",
-        number: "12-43-234345"
-    },
-    {
-        id : 4,
-        name: "Mary Poppendick",
-        number: "39-23-6423122"
-    }
-]
-
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
 })
   
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+    Person.find({}).then(result => {
+        console.log(result)
+        
+        res.json(result)
+    })
 })
 
 app.get('/api/persons/:id', (req, res) => {
-    const person = persons.find(person => person.id === Number(req.params.id))
-
-    if (person) {
-        res.json(person)
-    } else {
-        res.status(404).end()
-    }
+    
+    Person.findById(req.params.id).then(person => {
+        if (person) {
+            res.json(person)
+        } else {
+            res.status(404).end()
+        }
+    })
 })
 
 app.get('/info', (req, res) => {
-    res.send(`<div><p>Phone book has info for ${persons.length} people<p/><p>${new Date()}<p/>`)
+    Person.countDocuments((error, count) => {
+        if (error) {
+            console.log(error)
+            res.status(400).json(error)
+        } else {
+            console.log(count)
+            res.send(`<div><p>Phone book has info for ${count} people<p/><p>${new Date()}<p/>`)
+        }
+    })
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-    persons = persons.filter(person => person.id !== Number(req.params.id))
-
-    res.status(204).end()
+    Person.findOneAndDelete({_id: req.params.id}, (error, doc) => {
+        if (error) {
+            console.log(error)
+            res.status(400).json(error)
+        } else {
+            console.log(doc)
+            res.status(204).end()
+        }
+    })
 })
 
-const verify = ({number, name}) => {
-    if (persons.map(person => person.name).includes(name)) {
-        return {error: true, msg: "name must be unique"}
-    }
-    if (!number) {
-        return {error: true, msg: "missing number"}
-    }
-    if (!name) {
-        return {error: true, msg: "missing name"}
-    }
-    return {error: false, msg: null}
-}
 
 app.put('/api/persons/:id', (req, res) => {
     const number = req.body.number
     const id = req.params.id
     
-    if (!persons.map(person => person.id).includes(Number(id))) {
-        return res.status(400).json({
-            error: "No persons with parameter id"
-        })
-    }
-
-    const personNew = persons.find(person => person.id === id)
-    personNew.number = number
-
-    res.json(person)
+    Person.findOneAndUpdate({_id: id}, {number: number}, {new: true  }, (error, doc) => {
+        if (error) {
+            console.log(error)
+            res.status(400).json(error)       
+        } else {
+            console.log(doc)
+            res.json(doc)
+        }
+    })
 })
 
 app.post('/api/persons', (req, res) => {
     const body = req.body
-    
-    const result = verify(body)
-    if (result.error) {
-        return res.status(400).json({
-            error: result.msg
-        })
-    }
 
-    const person = {
-        id: Math.floor(Math.random()*9999999999),
+    const person = new Person({
         name: body.name,
-        number: body.number
-    }
+        number: body.number,
+    })
 
-    persons = persons.concat(person)
-    res.json(person)
+    person.save().then(savedPerson => {
+        res.json(savedPerson)
+    })
 })
 
 
